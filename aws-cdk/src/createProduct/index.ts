4ad51af88fd901dfd29d2ea14c6a3b0eb8ba5cd5
werinolic <drozdovpavel1997@gmail.com>
2024-06-24 00:57:57 +0300

    feat: add validation

--- aws-cdk/src/createProduct/index.ts
+++ aws-cdk/src/createProduct/index.ts
@@ -2,26 +2,35 @@ import { APIGatewayProxyHandler } from 'aws-lambda';
 import * as AWS from 'aws-sdk';
 
 const db = new AWS.DynamoDB.DocumentClient();
-function uuidv4() {
-  var u='',m='xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx',i=0,rb=Math.random()*0xffffffff|0;
+const uuidv4 = () => {
+  let u='',m='xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx',i=0,rb=Math.random()*0xffffffff|0;
   while(i++<36) {
-    var c=m[i-1],r=rb&0xf,v=c=='x'?r:(r&0x3|0x8);
-    u+=(c=='-'||c=='4')?c:v.toString(16);rb=i%8==0?Math.random()*0xffffffff|0:rb>>4
+    const c=m[i-1],r=rb&0xf,v=c=='x'?r:(r&0x3|0x8);
+    u+=(c=='-'||c=='4')?c:v.toString(16);rb=i%8==0?Math.random()*0xffffffff|0:rb>>4;
   }
-  return u
+  return u;
 }
 export const handler: APIGatewayProxyHandler = async (event) => {
   const { name, title, description, price, count } = JSON.parse(event.body || '{}');
+
+  if (
+    !name || typeof name !== "string" ||
+    !title || typeof title !== "string" ||
+    !description || typeof description !== "string" ||
+    !price || typeof price !== "number" ||
+    !count || typeof count !== "number"
+  ) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: "Missing required fields or data is invalid" })
+    };
+  }
+
   const productsTable = process.env.PRODUCTS_TABLE_NAME!;
   const stocksTable = process.env.STOCKS_TABLE_NAME!;
   const productId = uuidv4();
 
-  if (!productId || !name || !title || !description || !price || !count) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ message: "Missing required fields" })
-    };
-  }
+
 
   const productItem = { id: productId, name,title,description, price };
   const stockItem = { productId, count };
